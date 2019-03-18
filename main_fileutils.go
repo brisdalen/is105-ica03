0b5ce10d5d5dc435a2a6b923e40a7f6dbb7123f0
Nosp1 <Trymstaur@hotmail.com>
2019-03-18 13:37:27 +0100

    oppgave 5b tcp server og client

--- main_fileutils.go
+++ main_fileutils.go
@@ -7,18 +7,11 @@ import (
 
 	"./fileutils"
 )
-<<<<<<< HEAD
 
-var c = "./files/text1.txt"
-var d = "./files/text2.txt"
-var a = fileutils.FileToByteslice(c)
-var b = fileutils.FileToByteslice(d)
-=======
 var pathA = "./files/text1.txt"
 var pathB = "./files/text2.txt"
 var a = fileutils.FileToByteslice(pathA)
 var b = fileutils.FileToByteslice(pathB)
->>>>>>> eb4f1803950c16f4de5919e9f071a06ea3f1aca4
 
 func main() {
 	fmt.Print(a)
--- main_tcp_server.go
+++ main_tcp_server.go
@@ -1,20 +1,37 @@
 package main
 
-import "net"
+import (
+	"encoding/json"
+	"net"
+)
 
-func handler(c net.Conn) {
-	c.Write([]byte("ok"))
+type nameAndEmail struct {
+	Name string
+	Email string
+}
+
+
+
+func handler(c net.Conn, msg nameAndEmail) {
+	response, _ := json.Marshal(msg)
+	c.Write([]byte(response))
+
 	c.Close()
+
 }
 
 func main() {
 	l, err := net.Listen("tcp", ":5000")
 	if err != nil{
 		panic(err) }
+	response := nameAndEmail{
+		Name: "Bjørnar",
+		Email: "bjørnar@bjørnar.gmail.com"}
 	for {
 		c, err := l.Accept()
 		if err!=nil{
 			continue }
-		go handler(c)
+		go handler(c, response)
+
 	}
 }
