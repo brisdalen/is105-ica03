b47b09b1d040ca8900215dacec6e93cb0c932c91
bjornar.risdalen <bwendelborgrisdalen@gmail.com>
2019-03-21 16:30:24 +0100

    Added http server and client. Further organization

---
+++ servers/http_server/main_http_server.go
@@ -0,0 +1,17 @@
+package main
+
+import(
+	"fmt"
+	"net/http"
+	"../jsonify"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	msg := jsonify.Encode("bjørnar", "bjørnar@bjørnar.gmail.com")
+	fmt.Fprintf(w, string(msg))
+}
+
+func main() {
+	http.HandleFunc("/connected", handler)
+	http.ListenAndServe(":8765", nil)
+}
