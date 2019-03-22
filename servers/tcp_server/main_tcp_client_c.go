d6c9a2a22ea183c0c6054a74513e8f87ec349278
bjornar.risdalen <bwendelborgrisdalen@gmail.com>
2019-03-22 12:52:10 +0100

    Put all things JSON into jsonify.go, changed the client in client_c.go to take a command-line argument for the IP address.

--- servers/tcp_server/main_tcp_client_c.go
+++ servers/tcp_server/main_tcp_client_c.go
@@ -4,16 +4,24 @@ import (
 	"fmt"
 	"net"
 	"bufio"
+	"os"
 )
 // used for 5c
 func main() {
-	conn, err := net.Dial("tcp", "192.168.43.243:8765")
-	if err != nil {
-		fmt.Println("Error")
+	args := os.Args
+	if len(args) == 1 {
+		fmt.Println("Did you forget the IP address?")
+	} else {
+		ip := os.Args[1] + ":8765"
+		conn, err := net.Dial("tcp", ip)
+		if err != nil {
+			fmt.Println("Error")
+		}
+
+		fmt.Fprintf(conn, "GET / HTTP/1.0\n\n")
+		status, err := bufio.NewReader(conn).ReadString('\n')
+		fmt.Println(status)
 	}
 
-	fmt.Fprintf(conn, "GET / HTTP/1.0\n\n")
-	status, err := bufio.NewReader(conn).ReadString('\n')
-	fmt.Println(status)
-
 }
+
