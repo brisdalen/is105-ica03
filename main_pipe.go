c4a3d2a67fec1bac1b5e54be021adc7ab211a84d
Henrikohlsen <Henros18@uia.no>
2019-04-03 15:38:57 +0200

    added more lines to pipe

--- main_pipe.go
+++ main_pipe.go
@@ -1,18 +1,34 @@
-
 package main
 
 import (
 	"fmt"
 	"github.com/Henrikohlsen/Is-105/is105-ica03/pipe"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
-	hex := pipe.HexReturn("Hallo")
-	fmt.Println(hex, "\n")
-	b64 := pipe.Base64Return(hex)
-	fmt.Println(b64, "\n")
-	gzip := pipe.GZipReturn(b64)
-	fmt.Println(gzip)
+	text, err := ioutil.ReadFile("files/pg100.txt")
+
+	ts, _ := os.Stat("files/pg100.txt")
+	size := ts.Size()
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-
+	if size <= 100000 {
+		PrintValues(text)
+	} else {
+		fmt.Println("The file is currently to large")
+	}
+}
+func PrintValues(b []byte) {
+	hex := pipe.HexReturn(string(b))
+	fmt.Println(hex, "\n", "This Hexcode length is ", len(hex), "in bytes", "\n")
+	b64 := pipe.Base64Return(hex)
+	fmt.Println(b64, "\n", "This Base64 length is", len(b64), "in bytes", "\n")
+	gzip := pipe.GZipReturn(b64)
+	fmt.Println(gzip, "\n", "This GZIP length is", len(gzip), "in bytes")
+}
