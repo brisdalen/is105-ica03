dd725f037e4f2868c8946a500065bab506abfe47
bjornar.risdalen <bwendelborgrisdalen@gmail.com>
2019-04-24 12:26:11 +0200

    Added a way to get strings with new version number

---
+++ fileversion/fileversion.go
@@ -0,0 +1,33 @@
+package fileversion
+
+import (
+	"fmt"
+	"strconv"
+	"unicode"
+)
+/**
+	Lager nytt filnavn med nytt nummer.
+	Funksjonen antar at .txt er i bruk.
+	For eksempel: lang1.txt -> lang2.txt
+	Ledende 0'er blir fjernet: lang01.txt -> lang2.txt
+ */
+func DontOverrideFileversion(filename string) string {
+	s := filename[:len(filename)-4]
+
+	var id int
+
+	fmt.Println(len(s))
+	for i := len(s); i > 0; i-- {
+		fmt.Println(i)
+		if unicode.IsLetter(rune(s[i-1])) {
+			fmt.Println("Found!", i)
+			id, _ = strconv.Atoi(s[i:])
+			s = s[:i]
+			break
+		}
+	}
+
+	id++
+	s = s + strconv.Itoa(id) + ".txt"
+	return s
+}
