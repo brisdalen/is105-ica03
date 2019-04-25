c8c86b93ed8abe61e04963625a18f373fc18f939
Nosp1 <Trymstaur@hotmail.com>
2019-04-25 14:41:33 +0200

    elendelig lagring til fil

--- servers/tcp_server/main_tcp_server_b.go
+++ servers/tcp_server/main_tcp_server_b.go
@@ -18,7 +18,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	response := jsonify.Encode("bjørnar", "bjørnar@bjørnar.gmail.com")
+	response := jsonify.Encode("trym", "trymerkul@gmail.com")
 	for {
 		c, err := l.Accept()
 		if err != nil {
